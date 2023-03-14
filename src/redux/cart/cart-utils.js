11c1b0c28fb0889e14ccc2c9ee2b6f3160bf32f3
Lucas Voltera <lucasvoltera@gmail.com>
2023-03-14 17:48:25 -0300

    adding remove items function in checkout page

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -16,3 +16,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
      // if the item doesnt exist, create add the new item to the end of the array
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   };
+
+
+  export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToRemove.id
+    );
+  
+    if (existingCartItem.quantity === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+  
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  };
